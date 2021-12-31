a5f9c63e30c3b06453dadc5a5df8c1cc41c4690d
Dieter Stockhausen <dieter@schwingenhausen.at>
2021-12-31 08:05:51 +0100

    Fix install script windows if started on UNC Path.

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 set SCRIPT_DIR=%~dp0
 set VERSION=1.0.2.3
 ::
-cd /d %SCRIPT_DIR%target
-powershell -command "Expand-Archive -Force 'LRPureRAW%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%'"
+powershell -command "Expand-Archive -Force '%SCRIPT_DIR%target\LRPureRAW%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%'"
+pause
 
