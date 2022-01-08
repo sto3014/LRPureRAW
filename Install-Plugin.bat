1133d9e7429bb919720765170ef2ce5bce9aba83
Dieter Stockhausen <dieter@schwingenhausen.at>
2022-01-08 09:28:15 +0100

    Fix installation

--- Install-Plugin.bat
+++ Install-Plugin.bat
@@ -2,6 +2,6 @@
 set SCRIPT_DIR=%~dp0
 set VERSION=1.5.0.0
 ::
-powershell -command "Expand-Archive -Force '%SCRIPT_DIR%target\LRPureRAW%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%'"
+powershell -command "Expand-Archive -Force '%SCRIPT_DIR%LRPureRAW%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%'"
 pause
 
